a5eb97508f021ca67f455d7fb641a00db62ca51e
Alexandr Tcikin <av@sunnyday.dev>
2023-05-16 08:23:30 +0300

    Fixed output file writing

--- test-impact-gradle-plugin/src/main/kotlin/dev/sunnyday/test/impact/plugin/task/testimpact/TestImpactTaskOutput.kt
+++ test-impact-gradle-plugin/src/main/kotlin/dev/sunnyday/test/impact/plugin/task/testimpact/TestImpactTaskOutput.kt
@@ -2,6 +2,7 @@ package dev.sunnyday.test.impact.plugin.task.testimpact
 
 import dev.sunnyday.test.impact.plugin.domain.graph.ImpactProjectGraph
 import java.io.File
+import java.io.IOException
 import java.io.ObjectInputStream
 import java.io.ObjectOutputStream
 
@@ -10,7 +11,9 @@ internal class TestImpactTaskOutput(
 ) {
 
     fun writeImpactGraph(graph: ImpactProjectGraph) {
-        file.mkdirs()
+        if (!file.parentFile.mkdirs()) {
+            throw IOException("Can't create output file directory: ${file.parentFile}")
+        }
 
         ObjectOutputStream(file.outputStream()).use { stream ->
             stream.writeObject(graph)
